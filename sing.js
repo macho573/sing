82c5fcc131193b647e9ad5219eb08e080bfd4c3c
Gleb Barinov <112802459+macho573@users.noreply.github.com>
2022-09-11 11:49:19 +0300

    Add files via upload

---
+++ sing.js
@@ -0,0 +1,12 @@
+for(count = 5; count > 0; count--);{
+    alert("5 bottles of beer on the wall, 5 bottles of beer, If one of those bottles should happen to fall, 4 bottles of beer on the wall");
+    if(count = 4, count--);
+    alert("4 bottles of beer on the wall, 4 bottles of beer, If one of those bottles should happen to fall, 3 bottles of beer on the wall");
+    if(count = 3, count--);
+    alert("3 bottles of beer on the wall, 3 bottles of beer, If one of those bottles should happen to fall, 2 bottles of beer on the wall");
+    if(count = 2, count--);
+    alert("2 bottles of beer on the wall, 2 bottles of beer, If one of those bottles should happen to fall, 1 bottle of beer on the wall");
+    if(count = 1, count--);
+    alert("1 bottle of beer on the wall, 1 bottle of beer, If one of those bottles should happen to fall, No more bottles of beer on the wall, no more bottles of beer. Theres nothing else to fall, because theres no more bottles of beer on the wall.");
+    
+    }
